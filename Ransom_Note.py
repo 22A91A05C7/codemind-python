0805d7d89a8a9b8351f09eabab04d94fcdd4cc1d
22A91A05C7 <119839410+22A91A05C7@users.noreply.github.com>
2023-06-08 20:57:10 +0530

    def num(x,y):     for i in x:         if i in y:             y.remove(i)         else:             return False     return True x,y=map(list,input().split()) print(num(x,y))

--- Ransom_Note.py
+++ Ransom_Note.py
@@ -1,17 +1,9 @@
 def num(x,y):
-
     for i in x:
-
         if i in y:
-
             y.remove(i)
-
         else:
-
             return False
-
     return True
-
 x,y=map(list,input().split())
-
 print(num(x,y))
